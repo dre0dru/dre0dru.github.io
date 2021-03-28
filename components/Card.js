19e1d9de5f1dadc0d43818a783475ede2a04cdef
Andrey Skobelev <dre0druofficial@gmail.com>
2021-03-28 13:01:01 +0300

    replaced next-image usages with own image component

--- components/Card.js
+++ components/Card.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from '@/components/DefaultImage'
 import Link from '@/components/Link'
 
 const Card = ({ title, description, imgSrc, href }) => (
---
+++ components/DefaultImage.js
@@ -0,0 +1,5 @@
+const Image = ({ alt, src, className, width, height }) => {
+  return <img alt={alt} src={src} className={className} width={width} height={height} />
+}
+
+export default Image
