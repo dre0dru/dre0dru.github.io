20edd64022dbbc8c5ec99d7e008e13d3d99fd6c5
Andrey Skobelev <dre0druofficial@gmail.com>
2021-03-28 15:34:33 +0300

    adjusted projects layout

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,21 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Test',
+    description: `Test description`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/code-sample',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
+    title: 'Test1',
+    description: `Test description`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: '/blog/code-sample',
+  },
+  {
+    title: 'Test2',
+    description: `Test description`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/code-sample',
   },
 ]
 
