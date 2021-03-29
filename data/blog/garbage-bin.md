f34b4d44fe53c0d779618b6319d9bcde33b4d0ab
Andrey Skobelev <dre0druofficial@gmail.com>
2021-03-29 23:45:13 +0300

    ported 3 projects from old blog

---
+++ data/blog/garbage-bin.md
@@ -0,0 +1,28 @@
+---
+title: My fancy title
+date: '2021-01-31'
+tags: ['hello']
+draft: true
+summary:
+images: []
+---
+
+Draft post which should not display
+
+<div className="flex flex-wrap -mx-2 overflow-hidden xl:-mx-2">
+  <div className="my-1 px-2 w-full overflow-hidden xl:my-1 xl:px-2 xl:w-1/2">
+    <Image alt="Park Tupoley" src="/static/images/portfolio/park_tu_1.png" width={1286} height={964} />
+  </div>
+  <div className="my-1 px-2 w-full overflow-hidden xl:my-1 xl:px-2 xl:w-1/2">
+    <Image alt="Park Tupoley" src="/static/images/portfolio/park_tu_2.png" width={1286} height={964} />
+  </div>
+  <div className="my-1 px-2 w-full overflow-hidden xl:my-1 xl:px-2 xl:w-1/2">
+    <Image alt="Park Tupoley" src="/static/images/portfolio/park_tu_3.png" width={1286} height={964} />
+  </div>
+  <div className="my-1 px-2 w-full overflow-hidden xl:my-1 xl:px-2 xl:w-1/2">
+    <Image alt="Park Tupoley" src="/static/images/portfolio/park_tu_4.png" width={1286} height={964} />
+  </div>
+</div>
+
+<video controls="controls" loop="loop" src="../static/videos/ar_kai.mp4">
+                                </video>
--- data/projectsData.js
+++ data/projectsData.js
@@ -1,21 +1,21 @@
 const projectsData = [
   {
-    title: 'Test',
-    description: `Test description`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/code-sample',
+    title: 'Park Tupoley',
+    description: `This application is an augmented reality addition to the interactive exhibition "Park Tupoley", located in the KNITU-KAI campus.`,
+    imgSrc: '/static/images/portfolio/park_tu_3.png',
+    href: '/blog/park-tupoley',
   },
   {
-    title: 'Test1',
-    description: `Test description`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/code-sample',
+    title: 'No-spa forte',
+    description: `Marketing solution for the pharmaceutical company Sanofi (France).`,
+    imgSrc: '/static/images/social-banner.png',
+    href: '/blog/no-spa-forte',
   },
   {
-    title: 'Test2',
-    description: `Test description`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/code-sample',
+    title: 'KNITU-KAI admission campaign',
+    description: `Admission campaign for the 2019/2020 school year - augmented reality for the brochure of Kazan National Technical University KNITU-KAI.`,
+    imgSrc: '/static/images/social-banner.png',
+    href: '/blog/kai-admission-campaign',
   },
 ]
 
