77a039e61048499596e9e44043c384a46cefd6de
Andrey Skobelev <dre0druofficial@gmail.com>
2021-03-30 17:45:23 +0300

    ported remaining portfolio projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,23 @@
 const projectsData = [
   {
+    title: 'Tu-144',
+    description: `An interactive museum inside legendary supersonic aircraft.`,
+    imgSrc: '/static/images/social-banner.png',
+    href: '/blog/tu-144',
+  },
+  {
+    title: 'Military museum',
+    description: `Augmenting maquettes with digital content in Military Museum situated in Moscow.`,
+    imgSrc: '/static/images/social-banner.png',
+    href: '/blog/military-museum',
+  },
+  {
+    title: 'Ecocenter',
+    description: `Ecocenter is a space located in the heart of the Gorkinsky-Ometyevsky forest of Kazan, in which children receive important environmental knowledge in an interactive way.`,
+    imgSrc: '/static/images/portfolio/ecocenter.jpg',
+    href: '/blog/ecocenter',
+  },
+  {
     title: 'Park Tupoley',
     description: `This application is an augmented reality addition to the interactive exhibition "Park Tupoley", located in the KNITU-KAI campus.`,
     imgSrc: '/static/images/portfolio/park_tu_3.png',
